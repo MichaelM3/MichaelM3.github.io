21f15b159901554c76d2e4ae75fe50c12a46df4c
michaelm3 <michael.muniz94@gmail.com>
2022-05-21 12:06:59 -0400

    fixed build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MichaelM3.github.io/',
+  // base: '/MichaelM3.github.io/',
   plugins: [react()]
 })
