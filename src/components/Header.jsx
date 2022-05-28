b31be352e798921913c9dc656f88d01d4bab07a0
MichaelM3 <michael.muniz94@gmail.com>
2022-05-28 17:40:58 -0400

    changed icon package being used

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import me from '../me.jpg'
-import { MoonIcon} from '@heroicons/react/solid'
-import { SunIcon } from '@heroicons/react/outline'
+import { FaSun, FaMoon } from 'react-icons/fa'
 
 const Header = ({ theme, setTheme }) => {
 
@@ -19,9 +18,9 @@ const Header = ({ theme, setTheme }) => {
       <span className='flex'>
         <h1 className='text-black dark:text-slate-200 text-5xl'>Michael Muniz</h1>
         {theme === "light" ?
-          <SunIcon onClick={handleClick} className='w-12 h-12 absolute right-12 hover:cursor-pointer'/>
+          <FaSun onClick={handleClick} size={36} className='absolute right-12 hover:cursor-pointer'/>
           :
-          <MoonIcon onClick={handleClick} className='w-12 h-12 absolute right-12 hover:cursor-pointer' />
+          <FaMoon onClick={handleClick} size={36} className='absolute right-12 hover:cursor-pointer' />
         }
       </span>
       <p className='dark:text-white w-1/3'>
