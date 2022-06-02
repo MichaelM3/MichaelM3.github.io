4993ee05333a084495f55fa3e5e5dd3c254b4973
michaelm3 <michael.muniz94@gmail.com>
2022-06-02 13:53:26 -0400

    added modal for resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react'
 import resume from '../resumePic.jpg'
 
-const SoftSkills = () => {
+const Resume = () => {
+
+  const [showModal, setShowModal] = useState(false)
+
+  const handleCloseModal = (e) => {
+    if (e.target.id === "modal") {
+      setShowModal(false)
+    }
+  }
+
   return (
     <div className="flex flex-col items-center w-1/3 h-full">
       <h2 className="text-4xl dark:text-white mb-6">Resume</h2>
-      <img src={resume} className="overflow-hidden"/>
+      <img src={resume} onClick={() => setShowModal(true)} className="overflow-hidden hover:cursor-zoom-in transition duration-300" />
+      { showModal &&
+        <div id="modal" onClick={handleCloseModal} className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-3xl flex justify-center items-center'>
+          <div className='bg-white p-2 rounded'>
+            <img src={resume} />
+          </div>
+        </div>
+      }
     </div>
   )
 }
 
-export default SoftSkills
+export default Resume 
